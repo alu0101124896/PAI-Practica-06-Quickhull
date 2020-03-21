990cd1ae6846b551d999a4d56e4336e54d3b9ef8
Sergio Tabares <alu0101124896.ull.edu.es>
2020-03-21 19:07:49 +0000

    JSDoc documentation added

--- src/quick-hull.js
+++ src/quick-hull.js
@@ -9,21 +9,59 @@
 
 "use strict"
 
+/**
+ * @description Function that generates random numbers between the given values
+ *
+ * @param {number} min_val - Minimum random value
+ * @param {number} max_val - Maximum random value
+ * @returns {number} Returns the random number generated
+ */
 function random(min_val, max_val) {
   return (Math.floor(Math.random() * (max_val - min_val)) + min_val);
 }
 
+/**
+ * @description Class representing a point
+ *
+ * @class Point
+ */
 class Point {
+
+  /**
+   * @description Constructor that creates an instance of Point and gives it a random color.
+   * 
+   * @param {number} [x=0] - X coordinate
+   * @param {number} [y=0] - Y coordinate
+   * @memberof Point
+   */
   constructor(x = 0, y = 0) {
     this.x = Number(x);
     this.y = Number(y);
     this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')'
   }
 
+  /**
+   * @description Getter function of attribute x
+   *
+   * @returns {number} Returns the X coordinate of the point
+   * @memberof Point
+   */
   get_x() { return this.x; }
 
+  /**
+   * @description Getter function of attribute y
+   *
+   * @returns {number} Returns the Y coordinate of the point
+   * @memberof Point
+   */
   get_y() { return this.y; }
 
+  /**
+   * @description Function that draws this point in canvas
+   *
+   * @param {*} CONTEXT - Canvas context
+   * @memberof Point
+   */
   draw(CONTEXT) {
     CONTEXT.fillStyle = this.color;
     CONTEXT.beginPath();
@@ -32,19 +70,40 @@ class Point {
   }
 }
 
+/**
+ * @description Function that calls draw function for each point at given array
+ *
+ * @param {array} points - Array of points
+ * @param {*} CONTEXT - Canvas context
+ */
 function drawPoints(points, CONTEXT) {
   points.forEach(point => { point.draw(CONTEXT); });
 }
 
-function drawLine(firstPoint, secondPoint, CONTEXT) {
+/**
+ * @description Function that draws a line between the two given points
+ *
+ * @param {Point} lineStartingPoint - Line starting point
+ * @param {Point} lineEndingPoint - Line ending point
+ * @param {*} CONTEXT - Canvas context
+ */
+function drawLine(lineStartingPoint, lineEndingPoint, CONTEXT) {
   CONTEXT.beginPath();
-  CONTEXT.moveTo(firstPoint.get_x(), firstPoint.get_y());
-  CONTEXT.lineTo(secondPoint.get_x(), secondPoint.get_y());
+  CONTEXT.moveTo(lineStartingPoint.get_x(), lineStartingPoint.get_y());
+  CONTEXT.lineTo(lineEndingPoint.get_x(), lineEndingPoint.get_y());
   CONTEXT.lineWidth = 3;
   CONTEXT.strokeStyle = 'red';
   CONTEXT.stroke();
 }
 
+/**
+ * @description Function that draws all points and the current state of the convex hull
+ *
+ * @param {array} convexHull - Array of points that forms the convex hull
+ * @param {array} points - Array of points
+ * @param {*} CONTEXT - Canvas context
+ * @param {*} CANVAS - Canvas
+ */
 function redrawAll(convexHull, points, CONTEXT, CANVAS) {
   CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height);
   drawPoints(points, CONTEXT);
@@ -54,6 +113,12 @@ function redrawAll(convexHull, points, CONTEXT, CANVAS) {
   drawLine(convexHull[convexHull.length - 1], convexHull[0], CONTEXT);
 }
 
+/**
+ * @description Function that finds the left most point
+ *
+ * @param {array} points - Array of points
+ * @returns {Point} Returns the left most point
+ */
 function findLeftMostPoint(points) {
   let leftMostPoint = points[0];
   points.forEach(point => {
@@ -66,6 +131,12 @@ function findLeftMostPoint(points) {
   return leftMostPoint;
 }
 
+/**
+ * @description Function that finds the right most point
+ *
+ * @param {array} points - Array of points
+ * @returns {Point} Returns the right most point
+ */
 function findRightMostPoint(points) {
   let rightMostPoint = points[0];
   points.forEach(point => {
@@ -78,27 +149,55 @@ function findRightMostPoint(points) {
   return rightMostPoint;
 }
 
-function distanceToLine(pointToCheck, leftMostPoint, rightMostPoint) {
-  return (((pointToCheck.get_y() - leftMostPoint.get_y()) * (rightMostPoint.get_x() - leftMostPoint.get_x())) -
-    ((rightMostPoint.get_y() - leftMostPoint.get_y()) * (pointToCheck.get_x() - leftMostPoint.get_x())));
+/**
+ * @description Function that calculates the distance between a given point and the line between the other two givens
+ *
+ * @param {Point} pointToCheck - Point to calculate the distance to the line
+ * @param {Point} lineStartingPoint - Line starting point
+ * @param {Point} lineEndingPoint - Line ending point
+ * @returns {number} Returns the distance calculated
+ */
+function distanceToLine(pointToCheck, lineStartingPoint, lineEndingPoint) {
+  return (((pointToCheck.get_y() - lineStartingPoint.get_y()) * (lineEndingPoint.get_x() - lineStartingPoint.get_x())) -
+    ((lineEndingPoint.get_y() - lineStartingPoint.get_y()) * (pointToCheck.get_x() - lineStartingPoint.get_x())));
 }
 
-function isInSide(pointToCheck, leftMostPoint, rightMostPoint) {
-  if (distanceToLine(pointToCheck, leftMostPoint, rightMostPoint) > 0) {
+/**
+ * @description Function that checks if a given point is on one side of the line or the other
+ *
+ * @param {Point} pointToCheck - Point to check the side
+ * @param {Point} lineStartingPoint - Line starting point
+ * @param {Point} lineEndingPoint - Line ending point
+ * @returns {number} Retuns a number depending of the side in which the point is
+ */
+function isInSide(pointToCheck, lineStartingPoint, lineEndingPoint) {
+  if (distanceToLine(pointToCheck, lineStartingPoint, lineEndingPoint) > 0) {
     return 1;
-  } else if (distanceToLine(pointToCheck, leftMostPoint, rightMostPoint) < 0) {
+  } else if (distanceToLine(pointToCheck, lineStartingPoint, lineEndingPoint) < 0) {
     return 2;
   } else {
     return 0;
   }
 }
 
-function recursiveFindHull(convexHull, allPoints, points, leftMostPoint, rightMostPoint, CONTEXT, CANVAS) {
+/**
+ * @description Function that searchs for points outside of the current convex hull
+ * 
+ * @param {array} convexHull - Array of points that forms the convex hull
+ * @param {array} allCanvasPoints - Array of all canvas points
+ * @param {array} points - Array of points
+ * @param {Point} leftMostPoint - Left most point of the current section of the convex hull
+ * @param {Point} rightMostPoint - Right most point of the current section of the convex hull
+ * @param {*} CONTEXT - Canvas context
+ * @param {*} CANVAS - Canvas
+ * @returns
+ */
+function recursiveFindHull(convexHull, allCanvasPoints, points, leftMostPoint, rightMostPoint, CONTEXT, CANVAS) {
   if (points.length === 0) {
     return;
   } else if (points.length === 1) {
     convexHull.splice(convexHull.indexOf(leftMostPoint), 0, points[0]);
-    redrawAll(convexHull, allPoints, CONTEXT, CANVAS);
+    redrawAll(convexHull, allCanvasPoints, CONTEXT, CANVAS);
   } else {
     let farthestPoint;
     let farthestPointDistance = 0
@@ -112,7 +211,7 @@ function recursiveFindHull(convexHull, allPoints, points, leftMostPoint, rightMo
     });
 
     convexHull.splice(convexHull.indexOf(leftMostPoint), 0, farthestPoint);
-    redrawAll(convexHull, allPoints, CONTEXT, CANVAS);
+    redrawAll(convexHull, allCanvasPoints, CONTEXT, CANVAS);
 
     let pointsInSide1 = [];
     let pointsInSide2 = [];
@@ -125,25 +224,32 @@ function recursiveFindHull(convexHull, allPoints, points, leftMostPoint, rightMo
       }
     });
 
-    recursiveFindHull(convexHull, allPoints, pointsInSide1, leftMostPoint, farthestPoint, CONTEXT, CANVAS);
-    recursiveFindHull(convexHull, allPoints, pointsInSide2, farthestPoint, rightMostPoint, CONTEXT, CANVAS);
+    recursiveFindHull(convexHull, allCanvasPoints, pointsInSide1, leftMostPoint, farthestPoint, CONTEXT, CANVAS);
+    recursiveFindHull(convexHull, allCanvasPoints, pointsInSide2, farthestPoint, rightMostPoint, CONTEXT, CANVAS);
   }
 }
 
-function recursiveQuickHull(points, CONTEXT, CANVAS) {
+/**
+ * @description Function that starts to search the convex hull of the given array of points
+ * 
+ * @param {array} allCanvasPoints - Array of all canvas points
+ * @param {*} CONTEXT - Canvas context
+ * @param {*} CANVAS - Canvas
+ */
+function recursiveQuickHull(allCanvasPoints, CONTEXT, CANVAS) {
   let convexHull = [];
 
-  let leftMostPoint = findLeftMostPoint(points);
-  let rightMostPoint = findRightMostPoint(points);
+  let leftMostPoint = findLeftMostPoint(allCanvasPoints);
+  let rightMostPoint = findRightMostPoint(allCanvasPoints);
 
   convexHull.push(leftMostPoint);
   convexHull.push(rightMostPoint);
-  redrawAll(convexHull, points, CONTEXT, CANVAS);
+  redrawAll(convexHull, allCanvasPoints, CONTEXT, CANVAS);
 
   let pointsInSide1 = [];
   let pointsInSide2 = [];
 
-  points.forEach(pointToCheck => {
+  allCanvasPoints.forEach(pointToCheck => {
     if (isInSide(pointToCheck, leftMostPoint, rightMostPoint) === 1) {
       pointsInSide1.push(pointToCheck);
     } else if (isInSide(pointToCheck, leftMostPoint, rightMostPoint) === 2) {
@@ -151,10 +257,13 @@ function recursiveQuickHull(points, CONTEXT, CANVAS) {
     }
   });
 
-  recursiveFindHull(convexHull, points, pointsInSide1, leftMostPoint, rightMostPoint, CONTEXT, CANVAS);
-  recursiveFindHull(convexHull, points, pointsInSide2, rightMostPoint, leftMostPoint, CONTEXT, CANVAS);
+  recursiveFindHull(convexHull, allCanvasPoints, pointsInSide1, leftMostPoint, rightMostPoint, CONTEXT, CANVAS);
+  recursiveFindHull(convexHull, allCanvasPoints, pointsInSide2, rightMostPoint, leftMostPoint, CONTEXT, CANVAS);
 }
 
+/**
+ * @description Function that starts the execution of the progran
+ */
 function main() {
   const CANVAS = document.getElementById("canvas");
   if (CANVAS.getContext) {
